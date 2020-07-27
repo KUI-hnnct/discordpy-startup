e2a85e9397f7cf7e165378f3b0dfec8acd9ed9d8
KUI-hnnct <68815537+KUI-hnnct@users.noreply.github.com>
2020-07-27 15:07:18 +0900

    OP-YES()

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,9 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def OPPAI(OP):
+    await OP.send('YEEEEEEEE!!!')
+    
 
 bot.run(token)

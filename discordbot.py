d50e71129d94aea70d59a6f53d3c5cf29a9e701e
KUI-hnnct <68815537+KUI-hnnct@users.noreply.github.com>
2020-07-27 19:09:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,9 +10,9 @@ async def on_message(message):
     x = message.content
     if message.author.bot:
         return
-    if "/ping" == x:
+    if message.content == "/ping"
         await message.channel.send("pong")
     if message.content == "OP":
-        await message.channel.send("YEEEEEEEEEEEE")  
+        await message.channel.send("YEEEEEEEEEEEE")
     
 bot.run(token)

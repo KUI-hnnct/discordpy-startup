d75ccf009ee64ac0093e45a6f60e554bca79f693
KUI-hnnct <68815537+KUI-hnnct@users.noreply.github.com>
2020-07-27 19:10:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,12 +5,12 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
+@bot.event
 async def on_message(message):
     x = message.content
     if message.author.bot:
         return
-    if message.content == "/ping"
+    if message.content == "/ping":
         await message.channel.send("pong")
     if message.content == "OP":
         await message.channel.send("YEEEEEEEEEEEE")
